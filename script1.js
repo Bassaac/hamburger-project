26e1fc945089f9e098fbe67e73e8bb8bcd8f8a78
Bassaac <robertsisaacrobo@gmail.com>
2022-08-15 05:03:18 -1100

    Initial commit

---
+++ script1.js
@@ -0,0 +1,9 @@
+function setNewImage() {
+    document.getElementById("hoverme").src = "ham2new.png";
+
+
+}
+
+function setOldImage() {
+    document.getElementById("hoverme").src = "food 2/Burger.png";
+}
